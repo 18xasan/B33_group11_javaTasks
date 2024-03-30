783879aa22c007a97ba36022ef3c4e04f1c4878c
atilboztepe <atilboztepe@gmail.com>
2024-03-30 10:38:19 -0400

    week 1 all done

---
+++ src/atil/week2/swapTwoNumbers.java
@@ -0,0 +1,13 @@
+package atil.week2;
+
+public class swapTwoNumbers {
+    public static void main(String[] args) {
+        //Swap two variable' values without using a third variable
+
+
+
+
+
+
+    }
+}
