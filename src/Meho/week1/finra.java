91f124b04d65dcce0628845ccf3dd5bb23ff14c4
Meho Odobasic <mfodobasic@gmail.com>
2024-03-31 13:54:20 -0400

    added solution week1

--- src/Meho/week1/finra.java
+++ src/Meho/week1/finra.java
@@ -9,21 +9,21 @@ public class finra {
         //for numbers which are a multiple of both 3 and 5, print "FINRA" instead of the number.
 
         FINRA();
+
     }
 
     public static void FINRA() {
-        String result = "";
-        for (int i = 1; i <= 30; i++) { //iterate from 1 to 30
-            if (i % 5 == 0 && i % 3 == 0) //multiple of both 3 and 5, print "FINRA" instead of the number.
-                result += "FINRA ";
-            else if (i % 5 == 0) //multiple of 5, print "RA" instead of the number.
-                result += "RA ";
-            else if (i % 3 == 0)
-                result += "FIN ";  //multiple of 3, print "FIN" instead of the number.
-            else
-                result += i + " ";
+
+        for (int i = 1; i <= 30; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FINRA ");
+            } else if (i % 3 == 0) {
+                System.out.print("FIN ");
+            } else if (i % 5 == 0) {
+                System.out.print("RA ");
+            } else {
+                System.out.print(i + " ");
+            }
         }
-        System.out.println(result);
     }
 }
-
