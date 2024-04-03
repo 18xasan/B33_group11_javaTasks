b01aa29d92be20958d13559dc0be3928700eb777
atilboztepe <atilboztepe@gmail.com>
2024-04-03 00:31:02 -0400

    week 2 all done

--- src/atil/week2/swapTwoNumbers.java
+++ src/atil/week2/swapTwoNumbers.java
@@ -5,9 +5,16 @@ public class swapTwoNumbers {
         //Swap two variable' values without using a third variable
 
 
+                int a = 5;
+                int b = 10;
 
+                System.out.println("Before swapping: a = " + a + ", b = " + b);
 
+                a = a + b;
+                b = a - b;
+                a = a + b;
 
+                System.out.println("After swapping: a = " + a + ", b = " + b);
+            }
 
-    }
-}
+        }
