4c4eadb7e2c384b2b97e45b7f92bf0cfb1031711
Liliia Iliash <kasandra9491@gmail.com>
2024-04-03 11:51:24 -0400

    changed week2

--- src/Lilia/week2/swapTwoNumbers.java
+++ src/Lilia/week2/swapTwoNumbers.java
@@ -4,15 +4,16 @@ public class swapTwoNumbers {
     public static void main(String[] args) {
         //Swap two variable' values without using a third variable
 
-        String var1 = "Liliia";
-        String var2 = "Iliash";
-        System.out.println(var1 + " " + var2);
+        int num1 = 20;
+        int num2 = 60;
+        System.out.println("num1: " + num1);
+        System.out.println("num2: " + num2);
 
-        var1 = var1 + var2;
-        var2 = var1.substring(0, (var1.length() -var2.length()));
-        var1 = var1.substring(var2.length());
-
-        System.out.println(var1 + " " + var2);
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+        System.out.println("num1: " + num1);
+        System.out.println("num2: " + num2);
 
     }
 }
