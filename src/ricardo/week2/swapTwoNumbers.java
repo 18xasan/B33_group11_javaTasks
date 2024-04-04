4f712c633c1979e1a6c30679665bb6dc20a1b2fb
AntonioSDET24 <rjc0106@outlook.com>
2024-04-04 09:54:05 -0400

    Solutions for Taks 2 updated

--- src/ricardo/week2/swapTwoNumbers.java
+++ src/ricardo/week2/swapTwoNumbers.java
@@ -4,9 +4,20 @@ public class swapTwoNumbers {
     public static void main(String[] args) {
         //Swap two variable' values without using a third variable
 
+        int a = 10;
+        int b = 20;
 
+        System.out.println("Before Swaping:");
+        System.out.println(" a = " + a);
+        System.out.println(" b = " + b);
 
+        a = a + b;
+        b = a - b;
+        a = a - b;
 
+        System.out.println("After Swaping:");
+        System.out.println(" a = " + a);
+        System.out.println(" b = " + b);
 
 
     }
