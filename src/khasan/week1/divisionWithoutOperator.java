765552c2aaabf647d42206117cec22eed4f34b7d
AntonioSDET24 <rjc0106@outlook.com>
2024-04-04 09:09:57 -0400

    Solutions for Taks Two

--- src/khasan/week1/divisionWithoutOperator.java
+++ src/khasan/week1/divisionWithoutOperator.java
@@ -3,11 +3,22 @@ package khasan.week1;
 public class divisionWithoutOperator {
     public static void main(String[] args) {
         //Write a method that can divide two numbers without using division operator
+        int a = 10;
+        int b = 20;
 
+        System.out.println("Before Swaping:");
+        System.out.println(" a = " + a);
+        System.out.println(" b = " + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After Swaping:");
+        System.out.println(" a = " + a);
+        System.out.println(" b = " + b);
 
     }
-
-
 
 
 }
