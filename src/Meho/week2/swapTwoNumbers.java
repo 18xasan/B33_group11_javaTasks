317e1cec9f3cdd5974e2d7edd40f96339550a1d5
Meho Odobasic <mfodobasic@gmail.com>
2024-04-16 13:34:51 -0400

    added solution week2

--- src/Meho/week2/swapTwoNumbers.java
+++ src/Meho/week2/swapTwoNumbers.java
@@ -4,10 +4,18 @@ public class swapTwoNumbers {
     public static void main(String[] args) {
         //Swap two variable' values without using a third variable
 
+        int a = 5;
+        int b = 10;
+        System.out.println("Before swapping:");
+        System.out.println("a = " + a + ", b = " + b);
 
+        a = a + b;
+        b = a - b;
+        a = a - b;
 
-
-
+        System.out.println("After swapping:");
+        System.out.println("a = " + a + ", b = " + b);
 
     }
 }
+
