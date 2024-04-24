1db22b6bb89296675b4710c7c6c847c6667f94cf
atilboztepe <atilboztepe@gmail.com>
2024-04-24 11:36:24 -0400

    week  3 all done

--- src/atil/week2/swapTwoNumbers.java
+++ src/atil/week2/swapTwoNumbers.java
@@ -5,7 +5,6 @@ public class swapTwoNumbers {
         //Swap two variable' values without using a third variable
 
 
-<<<<<<<<< Temporary merge branch 1
                 int a = 5;
                 int b = 10;
 
@@ -13,29 +12,10 @@ public class swapTwoNumbers {
 
                 a = a + b;
                 b = a - b;
-                a = a + b;
+                a = a - b;
 
                 System.out.println("After swapping: a = " + a + ", b = " + b);
             }
 
         }
-=========
-        swapTwoNumbers(5,10);
-    }
 
-
-    public static void swapTwoNumbers(int num1 , int num2){
-        int a = 5;
-        int b = 10;
-
-        System.out.println("Before swapping: a = " + a + ", b = " + b);
-
-        a = a + b;
-        b = a - b;
-        a = a - b;
-
-        System.out.println("After swapping: a = " + a + ", b = " + b);
-    }
-}
-
->>>>>>>>> Temporary merge branch 2
