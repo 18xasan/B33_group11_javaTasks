33df0afcb57755609e1fa3edbde909d77084ce12
zanarditgs <turkishairlinescrew@gmail.com>
2024-06-06 11:23:30 -0400

    week4 and 5 updated.

---
+++ src/huseyin/week5/findUnique.java
@@ -0,0 +1,27 @@
+package huseyin.week5;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class findUnique {
+    public static void main(String[] args) {
+        /*Write a return  method that can find the unique characters from the String
+        Ex:  unique("AAABBBCCCDEF")  ==>  "DEF";*/
+
+        System.out.println("findUnique(\"AAABBBCCCDEF\") = " + findUnique("AAABBBCCCDEF"));
+    }
+
+    public static String findUnique(String str) {
+
+        String result ="";
+        for (int i = 0; i < str.length(); i++) {
+
+            if (str.indexOf(str.charAt(i)) == str.lastIndexOf(str.charAt(i))) {
+                result += str.charAt(i);
+            }
+        }
+
+        return result;
+    }
+
+}
