c21ac1442b7f535ffa0baba3dc116d5131d68f48
kirpimarmot <cevikkursat@gmail.com>
2024-06-26 23:23:28 +0200

    week8-12 questions added

---
+++ src/Week12/mapMinValue.java
@@ -0,0 +1,13 @@
+package Week12;
+
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+//Write a method that can return the minimum value from ta map (DO NOT use sort method)
+
+public class mapMinValue {
+
+
+
+}
---
+++ src/week11/sortMapbyValues.java
@@ -0,0 +1,11 @@
+package week11;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+//Write a method that can sort the Map by values
+public class sortMapbyValues {
+
+
+}
