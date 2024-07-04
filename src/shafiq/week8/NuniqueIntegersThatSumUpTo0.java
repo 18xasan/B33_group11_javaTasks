4b54d240af23c4068f35b8e2cc5a3ba71af3cb13
shafiq bayat <bayat37@hotmail.com>
2024-07-04 10:52:33 -0400

    Add solutions for permutation combinations and unique integers summing to zero
    Added core methods to generate all possible permutations of an array and to calculate N unique integers that add up to zero. For permutation, a recursive approach is used where characters are swapped and backtracked. For unique integers, positive and negative pairs are used to sum up to zero. In case of odd N, zero is added to balance the sum.

--- src/shafiq/week8/NuniqueIntegersThatSumUpTo0.java
+++ src/shafiq/week8/NuniqueIntegersThatSumUpTo0.java
@@ -7,4 +7,48 @@ public class NuniqueIntegersThatSumUpTo0 {
     The answer [1, -1,1,3] would be incorrect (because value 1 occurs twice).
     For N = 3 one of the possible answers is [-1,0,1]  (but there are many more correct answers).*/
 
+    public static class Main {
+        public static void main(String[] args) {
+
+            int[] result = uniqueIntegers(10);
+            for (int value : result) {
+                System.out.print(value + " ");
+            }
+        }
+
+        public static int[] uniqueIntegers(int N) {
+            int[] arr = new int[N];
+            int half = N / 2;
+            for (int i = 0; i < half; i++) {
+                arr[i] = i + 1;
+                arr[N - 1 - i] = - (i + 1);
+            }
+            if (N % 2 != 0) {
+                arr[half] = 0;
+            }
+            return arr;
+        }
+    }
+
+
+
 }
+
+/*
+The idea is to create numbers and their negations so that they sum up to zero. To clarify with an example,
+if we consider numbers 1 and -1, the sum is 0. If we consider 2 and -2, again the sum is 0. Thus, we use integers
+and their negative counterparts to satisfy the conditions of the task.
+As the requirements of the task also state that all integers must be unique, our code ensures that no two numbers
+(positive or negative integers) are the same in the array.
+The code is divided into three stages:
+Initialization: We initialize an integer array arr of size N.
+Assigning Values:
+If the provided N is an even number, for the first half of the array, we assign each element with the corresponding
+index's value + 1 (since index starts from 0, adding 1 gives us a number starting from 1). For the second half of the
+array, we do the same but make the value negative.
+If N is an odd number, we have an extra element in the array. In this case, we fill up the array in the same manner
+as before up to (N - 1) elements. For the remaining one element, we put '0'. This is because adding 0 does not
+affect the sum.
+In the end, these processes ensure that we have an array with N different elements such that their sum equals 0.
+Return Result: Finally, the function returns the array arr.
+ */
--- src/shafiq/week8/permutationCombinations.java
+++ src/shafiq/week8/permutationCombinations.java
@@ -2,5 +2,36 @@ package shafiq.week8;
 
 public class permutationCombinations {
     //Given an array of 3 characters print all permutation combinations from the given characters
+    //various ways in which a set of items can be ordered or arranged
 
+    public static class Main {
+        public static void main(String[] args) {
+            char[] array = {'a', 'b', 'c','d'};
+            permute(array, 0, array.length);
+        }
+
+        private static void permute(char[] array, int l, int r) {
+            if (l == r) {
+                for (char c : array) {
+                    System.out.print(c);
+                }
+                System.out.println();
+            } else {
+                for (int i = l; i < r; i++) {
+                    // swap
+                    char temp = array[l];
+                    array[l] = array[i];
+                    array[i] = temp;
+
+                    // recursion
+                    permute(array, l+1, r);
+
+                    // backtrack
+                    temp = array[l];
+                    array[l] = array[i];
+                    array[i] = temp;
+                }
+            }
+        }
+    }
 }
