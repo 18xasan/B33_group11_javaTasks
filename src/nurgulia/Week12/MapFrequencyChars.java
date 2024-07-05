b13403afb28245c86379ecc8e617b7a38e91009b
ng9mur <ng.muratbekova@gmail.com>
2024-07-05 18:02:22 -0500

    added solutions weeks 6-12

--- src/nurgulia/Week12/MapFrequencyChars.java
+++ src/nurgulia/Week12/MapFrequencyChars.java
@@ -9,21 +9,19 @@ public class MapFrequencyChars {
 
     public static void main(String[] args) {
         System.out.println("printFrequencyOfChar(\"hhgutgbirroiie\") = " + printFrequencyOfChar("hhgutgbirroiie"));
+
+
+
     }
 
 
     public static Map<Character,Integer> printFrequencyOfChar(String str){
         Map<Character,Integer> map = new HashMap<>();
-        int count = 0;
-        for (int i = 0; i < str.length(); i++) {
-
-            if (str.charAt(i) == str.charAt(i)){
-                count++;
-                map.put(str.charAt(i),count);
+        for (char c : str.toCharArray()) {
+           map.put(c,map.getOrDefault(c,0) + 1);
 
             }
 
-        }
 
         return map;
     }
--- src/nurgulia/Week12/mapMinValue.java
+++ src/nurgulia/Week12/mapMinValue.java
@@ -1,11 +1,31 @@
 package nurgulia.Week12;
 
-//Write a method that can return the minimum value from ta map (DO NOT use sort method)
+//Write a method that can return the minimum value from the map (DO NOT use sort method)
 
+
+import java.util.HashMap;
 import java.util.Map;
 
 public class mapMinValue {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("apple", 50);
+        map.put("banana", 20);
+        map.put("cherry", 30);
+        map.put("date", 10);
 
+        System.out.println("findMinValue(map) = " + findMinValue(map));
 
+    }
 
+public static int findMinValue(Map<String,Integer> map){
+    // Initialize min with a large value to ensure any value in the map is smaller
+    int minValue = Integer.MAX_VALUE;
+    for (int value : map.values()) {
+        if (value < minValue){
+            minValue = value;
+        }
+    }
+    return minValue;
+}
 }
